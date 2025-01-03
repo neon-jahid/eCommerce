59f920992c37723f2647f59406ed9def3afde133
Jahid Hasan <133555863+neon-jahid@users.noreply.github.com>
2025-01-03 22:54:23 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { assets } from '../assets/assets';
 
 function Navbar() {
@@ -32,6 +32,35 @@ function Navbar() {
                 <img
                     className='w-5 cursor-pointer'
                     src={assets.search_icon}
+                    alt=''
+                />
+                <div className='group relative'>
+                    <img
+                        className='w-5 cursor-pointer'
+                        src={assets.profile_icon}
+                        alt=''
+                    />
+                    <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                        <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                            <p className='cursor-pointer hover:text-black'>My Profiles</p>
+                            <p className='cursor-pointer hover:text-black'>Orders</p>
+                            <p className='cursor-pointer hover:text-black'>Logout</p>
+                        </div>
+                    </div>
+                </div>
+                <Link
+                    to='/cart'
+                    className='relative'>
+                    <img
+                        className='w-5 min-w-5'
+                        src={assets.cart_icon}
+                        alt=''
+                    />
+                    <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[-8px]'>5</p>
+                </Link>
+                <img
+                    className='w-5 cursor-pointer sm:hidden'
+                    src={assets.menu_icon}
                     alt=''
                 />
             </div>
